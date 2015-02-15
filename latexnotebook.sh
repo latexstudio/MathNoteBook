e2148b14d29859e9cd2c47b4fe609a949c139904
Hendry Olson <sayhi@hendryolson.com>
2015-02-15 09:20:30 +0100

    Fixed link paths

--- latexnotebook.sh
+++ latexnotebook.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-pdflatex "/Users/markholson/Dropbox/+Projects//LatexFiles/MathNoteBook/20150215-084449-rs2.2N-MarksMathNotebook.tex"
-makeindex "/Users/markholson/Dropbox/+Projects//LatexFiles/MathNoteBook/20150215-084449-rs2.2N-MarksMathNotebook.idx" -s StyleInd.ist
-pdflatex "/Users/markholson/Dropbox/+Projects//LatexFiles/MathNoteBook/20150215-084449-rs2.2N-MarksMathNotebook.tex"
-biber "/Users/markholson/Dropbox/+Projects//LatexFiles/MathNoteBook/20150215-084449-rs2.2N-MarksMathNotebook.tex"
-pdflatex "/Users/markholson/Dropbox/+Projects//LatexFiles/MathNoteBook/20150215-084449-rs2.2N-MarksMathNotebook.tex"
-pdflatex "/Users/markholson/Dropbox/+Projects//LatexFiles/MathNoteBook/20150215-084449-rs2.2N-MarksMathNotebook.tex"
+pdflatex "/Users/markholson/Dropbox/+Projects/LatexFiles/MathNoteBook/20150215-084449-rs2.2N-MarksMathNotebook.tex"
+makeindex "/Users/markholson/Dropbox/+Projects/LatexFiles/MathNoteBook/20150215-084449-rs2.2N-MarksMathNotebook.idx" -s StyleInd.ist
+pdflatex "/Users/markholson/Dropbox/+Projects/LatexFiles/MathNoteBook/20150215-084449-rs2.2N-MarksMathNotebook.tex"
+biber "/Users/markholson/Dropbox/+Projects/LatexFiles/MathNoteBook/20150215-084449-rs2.2N-MarksMathNotebook.tex"
+pdflatex "/Users/markholson/Dropbox/+Projects/LatexFiles/MathNoteBook/20150215-084449-rs2.2N-MarksMathNotebook.tex"
+pdflatex "/Users/markholson/Dropbox/+Projects/LatexFiles/MathNoteBook/20150215-084449-rs2.2N-MarksMathNotebook.tex"
