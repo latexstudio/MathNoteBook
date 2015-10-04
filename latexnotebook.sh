8ee275a4c6ee8c3f4af192c3d2991fee68fa3f04
Hendry Olson <hendryolson@gmail.com>
2015-10-04 20:28:11 +0200

    Upgrade to SageMathCloud

--- latexnotebook.sh
+++ latexnotebook.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-pdflatex "/Users/markholson/Dropbox/+Projects/LatexFiles/MathNoteBook/20150312-163541-rs2.2N-MarksMathNotebook.tex"
-makeindex "/Users/markholson/Dropbox/+Projects/LatexFiles/MathNoteBook/20150312-163541-rs2.2N-MarksMathNotebook.idx -s StyleInd.ist"
-biber "/Users/markholson/Dropbox/+Projects/LatexFiles/MathNoteBook/20150312-163541-rs2.2N-MarksMathNotebook.tex"
-pdflatex "/Users/markholson/Dropbox/+Projects/LatexFiles/MathNoteBook/20150312-163541-rs2.2N-MarksMathNotebook.tex"
-pdflatex "/Users/markholson/Dropbox/+Projects/LatexFiles/MathNoteBook/20150312-163541-rs2.2N-MarksMathNotebook.tex"
+pdflatex "20150903-160354-rs2.2-MarksMathNotebook.tex"
+makeindex "20150903-160354-rs2.2-MarksMathNotebook.idx -s StyleInd.ist"
+biber "20150903-160354-rs2.2-MarksMathNotebook.tex"
+pdflatex "20150903-160354-rs2.2-MarksMathNotebook.tex"
+pdflatex "20150903-160354-rs2.2-MarksMathNotebook.tex"
+
+for FILENAME in ./*.tex
+
+do
+    OUTPUT="$(echo $FILENAME | sed 's/\(.*\)\..*/\1/')"
+    rm "$OUTPUT.aux" "$OUTPUT.log" "$OUTPUT.out" "$OUTPUT.synctex.gz" "$OUTPUT.sagetex.sage" "$OUTPUT.sagetex.sage.py" "$OUTPUT.sagetex.scmd" "$OUTPUT.sagetex.sout" "$OUTPUT.synctex.gz" "$OUTPUT.idx" "$OUTPUT.bcf" "$OUTPUT.run.xml" "$OUTPUT.toc" "$OUTPUT.ptc" "$OUTPUT.tex.blg" "$OUTPUT.bbl" "$OUTPUT.blg"
+done
+exit
 
 
